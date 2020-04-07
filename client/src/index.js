93d96b81a025fd0ee39bf71421c31ca24de4289b
Cardinale <Csar>
2020-04-07 14:54:20 +0200

    Contrée : - Tirage des rois (2)  --> Mise en place de la boucle coté serveur afin de pouvoir distribuer les mêmes cartes à chaque joueur et ce même avec des différences de vitesse de connexion (4)

--- client/src/index.js
+++ client/src/index.js
@@ -3,17 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 import Menu from './Menu';
 import {ContreeMenu, ContreeStart, ContreeUsername, ContreePlay} from './Contree';
 
 const routing = (
   <BrowserRouter>
+    <Switch>
       <Route path="/" exact component={Menu}/>
       <Route path="/Contree" exact component={ContreeMenu}/>
       <Route path="/Contree/Start" exact component={ContreeStart}/>
       <Route path="/Contree/Join/:ident" exact component={ContreeUsername}/>
       <Route path="/Contree/Play/:ident" exact component={ContreePlay}/>
+    </Switch>
   </BrowserRouter>
 )
 
